d22b923f9d2251e73c63aa4d47c66d2f16faa967
Marques Avery <averymarques4@gmail.com>
2020-08-20 19:35:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,6 +11,7 @@ class Anagram
   def match(anagram)
     # anagram_array = anagram.split(" ")
     anagram.collect do |a|
+      binding.pry
       a2 = a.split(//)
       word2 = @word.split(//)
       a2.sort == @word2.sort
